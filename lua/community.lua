4baad67e87efaa26e457a90a4d0dd8521a3166a2
Tuomas Rinne <tuomas.rin@hotmail.com>
2024-04-02 13:14:36 +0300

    add community plugins

--- lua/community.lua
+++ lua/community.lua
@@ -5,21 +5,38 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- Languages
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.cs" },
+  { import = "astrocommunity.pack.zig" },
   { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.pack.typescript-all-in-one" },
-  { import = "astrocommunity.pack.svelte" },
   { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.kotlin" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.fish" },
   { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  -- Frameworks
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.angular" },
+  { import = "astrocommunity.pack.vue" },
   { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.templ" },
+  -- Data formats
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.json" },
-  { import = "astrocommunity.pack.docker" },
-  { import = "astrocommunity.pack.yaml" },
-  { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.proto" },
   { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.html-css" },
+  -- Themes
   { import = "astrocommunity.colorscheme.nord-nvim" },
+  -- Other
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.sql" },
 }
