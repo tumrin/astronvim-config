ea3af77725298bdc0ee1f1db00d5eb95ae242c17
Tuomas Rinne <tuomas.rin@hotmail.com>
2024-07-25 20:24:35 +0300

    add zls to ensure installed

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -7,6 +7,7 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "rust_analyzer",
+        "zls",
       })
     end,
   },
