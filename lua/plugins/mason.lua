5d5529e401795dc8a137de2027dc3231ce446ff9
Tuomas Rinne <tuomas.rin@hotmail.com>
2024-10-30 09:06:49 +0200

    disable prettierd if no config is present in root

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -16,6 +16,19 @@ return {
     opts = {
       ensure_installed = {
         "gofumpt",
+        "prettierd",
+      },
+      handlers = {
+        -- for prettierd
+        prettierd = function()
+          require("null-ls").register(require("null-ls").builtins.formatting.prettierd.with {
+            condition = function(utils)
+              return utils.root_has_file ".prettierrc"
+                or utils.root_has_file ".prettierrc.json"
+                or utils.root_has_file ".prettierrc.js"
+            end,
+          })
+        end,
       },
     },
   },
