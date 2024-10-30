9394e7e0eeba2532accc3b066c290febedac53e0
Tuomas Rinne <tuomas.rin@hotmail.com>
2024-10-30 08:28:43 +0200

    update config files

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,35 +4,25 @@
 return {
   {
     "williamboman/mason-lspconfig.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        --"biome", Add biome here when it works with mason, until then install manually
+    opts = {
+      ensure_installed = {
         "rust_analyzer",
         "zls",
-      })
-    end,
+      },
+    },
   },
   {
     "jay-babu/mason-null-ls.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+    opts = {
+      ensure_installed = {
         "gofumpt",
-      })
-      opts.handlers = {
-        prettierd = function()
-          require("null-ls").register(require("null-ls").builtins.formatting.prettierd.with {
-            condition = function(utils)
-              return utils.root_has_file ".prettierrc"
-                or utils.root_has_file ".prettierrc.json"
-                or utils.root_has_file ".prettierrc.js"
-            end,
-          })
-        end,
-      }
-    end,
+      },
+    },
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
-    opts = function(_, opts) opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {}) end,
+    opts = {
+      ensure_installed = {},
+    },
   },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,15 @@
 if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
+-- Customize Treesitter
+
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts) opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {}) end,
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      -- add more arguments for adding more treesitter parsers
+    },
+  },
 }
