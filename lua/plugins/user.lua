ba7c0a0b404fe659ed86a0b0f0dd05d6a6c8bb73
Tuomas Rinne <tuomas.rin@hotmail.com>
2025-03-25 15:21:09 +0200

    add hop.nvim

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: missing-fields, missing-parameter
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- Here are some examples:
 
@@ -11,8 +12,28 @@ return {
     keys = {
       { "<leader>O", "<cmd>Outline<CR>", desc = "Toggle outline" },
     },
-    opts = {
-      -- Your setup opts here
+    opts = {},
+  },
+  {
+    "smoka7/hop.nvim",
+    opts = {},
+    dependencies = {
+      "AstroNvim/astrocore",
+      opts = {
+        mappings = {
+          n = {
+            ["gs"] = { function() require("hop").hint_words() end, desc = "Hop hint words" },
+            ["g<S-s>"] = { function() require("hop").hint_lines() end, desc = "Hop hint lines" },
+          },
+          v = {
+            ["s"] = { function() require("hop").hint_words { extend_visual = true } end, desc = "Hop hint words" },
+            ["<S-s>"] = {
+              function() require("hop").hint_lines { extend_visual = true } end,
+              desc = "Hop hint lines",
+            },
+          },
+        },
+      },
     },
   },
   ------Outline--------
