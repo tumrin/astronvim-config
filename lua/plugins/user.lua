46402238f5ad58296069fd27a1a967d876a76ad6
Tuomas Rinne <tuomas.rin@hotmail.com>
2025-03-28 13:22:11 +0200

    disable quickgd

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -14,7 +14,13 @@ return {
     },
     opts = {},
   },
-  ------Outline--------
+  ----Outline--------
+  ----Disable quickgd from godot community pack------
+  {
+    "QuickGD/quickgd.nvim",
+    enabled = false,
+  },
+  ----Disable quickgd from godot community pack------
   ------Hop------------
   {
     "smoka7/hop.nvim",
