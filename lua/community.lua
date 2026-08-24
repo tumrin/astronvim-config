2ef317a9865f654a6c6ce5ccb9fe87e364c71f34
Tuomas Rinne <tuomas.rin@protonmail.com>
2026-08-24 05:28:56 +0300

    add opencode-nvim

--- lua/community.lua
+++ lua/community.lua
@@ -52,4 +52,6 @@ return {
   { import = "astrocommunity.pack.terraform" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.editing-support.conform-nvim" },
+  -- AI
+  { import = "astrocommunity.ai.opencode-nvim" },
 }
